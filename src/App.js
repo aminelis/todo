bef0e8243600c5390d64230dabd22f22184f2701
amine <aminelissir1@gmail.com>
2023-07-27 22:12:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ListTask from "./components/ListTask";
+import AddTask from "./components/Addtask";
+import { Provider } from "react-redux";
+import store from './store/index'
+
+const App = () => {
+  return (
+    <Provider store={store}>
+    <div>
+    <h1>My Todo App</h1>
+      <AddTask />
+      <ListTask/>
+    </div>
+
+
+
+  </Provider>
+  );
+};
+
+export default App;
